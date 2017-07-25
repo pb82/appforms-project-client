e7910cf6703619dfb6945890e21bb861af4c0536
Peter Braun <pbraun@redhat.com>
2017-07-25 18:31:11 +0200

    rebuild template

--- www/main.js
+++ www/main.js
@@ -12,6 +12,55 @@ FormModel = Backbone.Model.extend({
         "active_page": null,
         "page_history": []
     },
+    unsafeFormFields: [
+        "name",
+        "helpText",
+        "fieldCode"
+    ],
+    // Escape form fields to prevent XSS attacks
+    sanitizeForm: function(form) {
+        var self = this;
+
+        if (!form.fields || form.fields.length === 0) {
+            return;
+        }
+
+        // Sanitize radio, dropdown and checkbox fields
+        function sanitizeMultipleChoice(field) {
+            var props = field.props;
+            if (props && props.fieldOptions && props.fieldOptions.definition) {
+                var definition = props.fieldOptions.definition;
+                if (definition.options) {
+                    for (var i = 0; i < definition.options.length; i++) {
+                        definition.options[i].label = _.escape(definition.options[i].label);
+                    }
+                }
+            }
+        }
+
+        // Sanitize default fields (text, number, etc.)
+        function sanitizeDefault(field) {
+            self.unsafeFormFields.forEach(function (prop) {
+                field.props[prop] = _.escape(field.props[prop]);
+            });
+        }
+
+        function sanitizeField(field) {
+            if (field.props) {
+                if (["radio", "dropdown", "checkboxes"].indexOf(field.props.type) >= 0) {
+                    // Multiple choice fields need special treatment
+                    sanitizeMultipleChoice(field);
+                }
+                sanitizeDefault(field);
+            }
+        }
+
+        for (var field in form.fields) {
+            if (form.fields.hasOwnProperty(field)) {
+                sanitizeField(form.fields[field]);
+            }
+        }
+    },
     loadForm: function() {
         var formId = this.get("formId");
         var self = this;
@@ -21,6 +70,7 @@ FormModel = Backbone.Model.extend({
             if (err) {
                 self.trigger("error", err);
             } else {
+                self.sanitizeForm(form);
                 self.coreModel = form;
                 self.set("fh_full_data_loaded", true);
                 self.id = formId;
